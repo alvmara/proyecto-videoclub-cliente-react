9e8b4064df93798ccf1e5c127fbe65069b1b1c98
alvmara <alvaromarzo104@gmail.com>
2022-03-10 19:49:24 +0100

    añadiendo imagen y creando estructura login y registro

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home/Home";
+import Login from "./Containers/Login/Login";
+import Registro from "./Containers/Registro/Registro";
+
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <ChakraProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Registro />} />
+          </Routes>
+        </ChakraProvider>
       </BrowserRouter>
     </div>
   );
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,10 +1,14 @@
+import { Container, Text } from '@chakra-ui/react'
 import React from 'react'
 
-
+import fondo from '../../img/fondo1.svg';
 
 function Home() {
   return (
-    <div>Home</div>
+    <Container size="xl" backgroundImage={fondo} h="100vh" maxWidth="80%" display="flex" flexDirection="column" justifyContent="center" alignItems="flex-end">
+        <Text fontSize='6xl' color='violet' fontWeight="bold" dropShadow="lg">Bienvenido al videoclub</Text>
+        <Text fontSize='2xl' color="black">Hazte usuario y disfruta del mejor contenido audiovisual</Text>
+    </Container>
   )
 }
 
