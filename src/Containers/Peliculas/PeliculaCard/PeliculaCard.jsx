bafe57a1110007c90c8a4e93b6c4e2603e177c83
alvmara <alvaromarzo104@gmail.com>
2022-03-12 19:38:07 +0100

    mostrando lista de peliculas y boton de ver mas

---
+++ src/Containers/Peliculas/PeliculaCard/PeliculaCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function PeliculaCard({ pelicula }) {
+    /**
+     * 
+     * adult: false
+        createdAt: "2022-03-10T17:06:39.000Z"
+        fecha: "2021-09-13T01:24:00.000Z"
+        id: 1
+        image: "https://image.tmdb.org/t/p/original//mmznhaQDwlHWpUwKuNxtQiubbmM.jpg"
+        popularity: 123.71
+        sinopsis: "A ragtag band of humans band together in the year 2025 to fight legions of monstrous creatures rising from the sea. Using massive piloted robots to combat the alien threat, earth's survivors take the fight to the invading alien force lurking in the depths of the Pacific Ocean. Nearly defenseless in the face of the relentless enemy, the forces of mankind have no choice but to turn to two unlikely heroes -- a washed up former pilot and an untested trainee -- who now stand as earth's final hope against the mounting apocalypse."
+        titulo: "Pacific Rim"
+        updatedAt: "2022-03-10T17:06:39.000Z"
+     */
+
+  return (
+    <div>
+        <img src={pelicula.image} alt={pelicula.titulo} />
+        <div>{pelicula.titulo}</div>
+        <div>{pelicula.updatedAt}</div>
+    </div>
+  )
+}
+
+export default PeliculaCard
--- src/Containers/Peliculas/Peliculas.jsx
+++ src/Containers/Peliculas/Peliculas.jsx
@@ -1,22 +1,26 @@
-import { Container, Text } from '@chakra-ui/react'
+import { Button, Container, Text } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import PeliculaCard from './PeliculaCard/PeliculaCard';
 
 const Peliculas = () => {
   const [pelis, setPelis] = useState([]);
+  const [indice, setIndice] = useState(20);
 
   useEffect(() => {
     axios.get("http://localhost:5500/peliculas")
       .then(res => setPelis(res.data));
   }, []);
 
-  useEffect(() => {
-    console.log(pelis);
-  }, [pelis]);
-
   return (
     <Container size="xl" h="100vh" maxWidth="80%" display="flex" flexDirection="column" justifyContent="center" alignItems="flex-end">
         <Text fontSize='6xl' color='violet' fontWeight="bold" dropShadow="lg">Peliculas</Text>
+
+        <ul>
+          {pelis.slice(0, indice).map(peli => <PeliculaCard pelicula={peli} />)}
+        </ul>
+
+        <Button onClick={() => setIndice(indice + 20)}>Ver más</Button>
     </Container>
   )
 }
